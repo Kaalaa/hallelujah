7d42c99bd732c788266058718914dbb9731d166b
Karla Schönicke <karla_schoenicke@yahoo.de>
2018-01-24 16:24:39 +0100

    4.5 Add carousel on landing page, create product partial

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,8 @@ class SimplePagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
   end
+  def landing_page
+    lim_product = Product.all.count/2
+    @products = Product.limit(lim_product)
+  end
 end
