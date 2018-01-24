312623ea0e5d2fc36c38f31468e1f9ef3687c0ae
Karla Schönicke <karla_schoenicke@yahoo.de>
2018-01-24 16:44:08 +0100

    4.5 Unsuccesful troubleshooting of multiple display on products page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,11 +1,11 @@
 class SimplePagesController < ApplicationController
   def index
   end
-  def landing_page
-    @featured_product = Product.first
-  end
+  # def landing_page
+  #   @featured_product = Product.first
+  # end
   def landing_page
     lim_product = Product.all.count/2
-    @products = Product.limit(lim_product)
+    @products = Product.limit(lim_product) #Somehow this is messing up my products page, because the 6 products are not just displayed ONCE but 6 times...
   end
 end
