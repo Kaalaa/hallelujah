049176e1fea69ec87aafcc64c947d1a73c95178e
Karla Schönicke <karla_schoenicke@yahoo.de>
2018-01-26 15:35:25 +0100

    5.3 Incorporate ActionMailer and Sendgrid

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,4 +8,13 @@ class SimplePagesController < ApplicationController
     lim_product = Product.all.count/2
     @products = Product.limit(lim_product)
   end
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'karla_schoenicke@yahoo.de',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
