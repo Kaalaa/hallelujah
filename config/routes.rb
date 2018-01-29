a6e53af652054550c77632a25b5f2ce30c311f22
Karla Schönicke <karla_schoenicke@yahoo.de>
2018-01-29 13:07:59 +0100

    5.4 Clean up for forking

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  post 'simple_pages/thank_you'
+  post 'simple_pages/thank_you' #this POST is the reason that we cannot directly call the thank_you URL in the browser
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
