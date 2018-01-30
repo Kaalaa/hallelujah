31ade9222f962d2a0ab6175bb426c8578d44718e
Karla Schönicke <karla_schoenicke@yahoo.de>
2018-01-30 18:49:20 +0100

    5.7 Add Comments model, introduce scope highest_rating lowest_rating

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :manage, Order, id: user.id # this should rescrict the orders but doesn't
   end
+
 end
